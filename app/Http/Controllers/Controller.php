d2f462221f4c854296eec7ee0af203afef77d333
bagasnusa <bagasnusatama3@domain.com>
2025-02-26 03:44:17 +0700

    Menambahkan beberapa controller dan update routes

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,12 +1,36 @@
 <?php
 
+
+
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, ValidatesRequests;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+}
+
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+        return 'Selamat Datang';
+    }
+
+    public function about()
+    {
+        return 'NIM: 123456789 - Nama: Bagas';
+    }
+
+    public function articles($id)
+    {
+        return "Halaman Artikel dengan ID {$id}";
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,40 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+// use Illuminate\Support\Facades\Route;
+
+// Route::get('/hello', function () {
+//     return 'Hello World';
+// });
+
+// Route::get('/world', function () {
+//     return 'World';
+// });
+
+// Route::get('/salam', function () {
+//     return 'Selamat Datang';
+// });
+
+// Route::get('/about', function () {
+//     return 'NIM: 123456789 - Nama: Bagas';
+// });
+
+// Route::get('/user/{name}', function ($name) {
+//     return 'Nama saya ' . $name;
+// });
+
+// Route::get('/posts/{post}/comments/{comment}', function ($postId, $comment) {
+//     return 'Pos ke- ' . $postId . ' Komentar ke- ' . $comment;
+// });
+
+// Route::get('/articles/{id}', function ($id) {
+//     return "Halaman Artikel dengan ID {$id}";
+// });
+
+// Route::get('/user/{name?}', function ($name='John') {
+//     return 'Nama saya ' .  $name;
+
+// });
+// Route:: get('/hello', [WelcomecController::class,'hello']);
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +47,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return 'Selamat Datang di Halaman Utama';
+// });
+use App\Http\Controllers\PhotoController;
+use Illuminate\Support\Facades\Route;
+
+Route::resource('photos', PhotoController::class);
+Route::get('/specialMahasiswa', function () {
+    return 'Special Mahasiswa';
 });
+Route::get('/specialUrl', function () {
+    return 'Special URL';
+});
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
+
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]);
